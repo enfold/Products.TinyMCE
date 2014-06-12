6353cd193f6066eaeda48898a45deff3f3c23d44
Franco Pellegrini <frapell@gmail.com>
2014-06-12 12:40:37 -0500

    Revert "This was already fixed in version 1.3.6 from commit 1f685d0f7956edfa2b4071259ce00d80923b7d28, so using that instead"
    This reverts commit 4331058a641a21752384b3ae2e20b26abe61710c.

--- Products/TinyMCE/browser/atanchors.py
+++ Products/TinyMCE/browser/atanchors.py
@@ -6,7 +6,7 @@ from zope.interface import implements
 try:
     from lxml.html import fromstring
     fromstring     # pyflakes
-    SEARCHPATTERN = ".//a"
+
 except ImportError:
     from elementtree import HTMLTreeBuilder
 
@@ -16,7 +16,8 @@ except ImportError:
         parser.feed(text)
         return parser.close()
 
-    SEARCHPATTERN = "*/a"
+SEARCHPATTERN = "*/a"
+
 
 class ATAnchorView(BrowserView):
     implements(IAnchorView)
